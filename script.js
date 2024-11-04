0a68cd53a74911624d6b90327910479bf1348839
aaditya_singh_21 <aaditya2110s@gmail.com>
2024-11-04 20:54:41 +0530

    Created functions for computer and user input and to check the winner

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function takeinput(){
-    
+const options = ["rock", "paper", "scissor"];
+
+// Taking the computer input
+function getComputerchoice(){
+    let choice = options[Math.floor(Math.random() * options.length)];
+    console.log(choice);
 }
+
+// checking for the winner
+function checkwinner(playerchoice, computerchoice){
+    if(playerchoice == computerchoice){
+        return "Draw!"
+    }
+    else if((playerchoice == "rock" && computerchoice == "scissor" || playerchoice == "paper" && computerchoice == "rock" 
+        || playerchoice == "scissor" && computerchoice == "paper")){
+            return "Player"
+    }
+    else{
+        return "Computer";
+    }
+}
+
+// taking user input
+function getHumanchoice(){
+    let validation = false;
+
+    while(validation == false){
+        let choice = prompt("Enter Rock, paper or scissor");
+        if(choice == null){
+            continue;
+        }
+        let playerchoice = choice.toLowerCase();
+        if(options.includes(playerchoice)){
+            validation = true;
+            return playerchoice;
+        }
+    }
+}
+
+getHumanchoice();
